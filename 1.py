2bb2ce5eb41bbbbdcb199c2a88920c0dea6d61e4
Rajpurohit Dhanpal <rajpurohitdhanpal@gmail.com>
2022-09-06 06:47:32 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("input.txt",'r')
+sys.stdout = open("output.txt", "w")
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(arr)
