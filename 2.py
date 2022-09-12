0ed3dc03612e13c7161f0101f0d2cb009f031728
Rajpurohit Dhanpal <rajpurohitdhanpal@gmail.com>
2022-09-12 06:34:32 +0530

    updated

--- 2.py
+++ 2.py
@@ -2,11 +2,12 @@ import sys
 sys.stdin = open("input.txt",'r')
 sys.stdout = open("output.txt", "w")
 
-t = unsigned(input())
-print(t)
-count = 0
-while t>0:
-	if (t&1==1):
-		count+= t & 1
-	t = t>>1
-print(count)
+num1 = [1,2,2,1]
+num2 = [2,2]
+result = []
+for i in num1:
+	if i in num2:
+		num2.remove(i)
+		result.append(i)
+
+print(result)
