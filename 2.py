d01fd94fe862c8887dd92bb7e7650fe41f935fbe
Rajpurohit Dhanpal <rajpurohitdhanpal@gmail.com>
2022-09-19 06:29:36 +0530

    permutation

--- 2.py
+++ 2.py
@@ -2,12 +2,15 @@ import sys
 sys.stdin = open("input.txt",'r')
 sys.stdout = open("output.txt", "w")
 
-num1 = [1,2,2,1]
-num2 = [2,2]
-result = []
-for i in num1:
-	if i in num2:
-		num2.remove(i)
-		result.append(i)
-
-print(result)
+def permute(nums: List[int]) -> List[List[int]]:
+    if len(nums)==0:
+        return []
+    if len(nums)==1:
+        return [nums]
+    lst = []
+    for i in range(len(nums)):
+        first = nums[i]
+        reminder = nums[:i]+nums[i+1:]
+        for p in permute(reminder):
+            lst.append([first]+p)
+    return lst
